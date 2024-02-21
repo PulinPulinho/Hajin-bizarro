0d316c5957a6d6ff9be2b94019705ba39b330af2
Pulin <113272580+PulinPulinho@users.noreply.github.com>
2024-02-20 22:13:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const nao = document.querySelector('#nao');
 const sim = document.querySelector('#sim');
 let sim_size = 10;
+let nao_size = 10;
 
 nao.onclick = () => {
     sim_size ++;
     sim.style.width = sim_size + 'vw';
+    sim.style.position = 'absolute';
+    nao_size --;
+    nao.style.width = nao_size + 'vw';
     nao.style.position = 'absolute';
     nao.style.left = '1vw';
     nao.style.top = (Math.floor(Math.random() * 50) + 1) + 'vh';
     nao.style.left = (Math.floor(Math.random() * 50) + 1) + 'vw';
 }
-
