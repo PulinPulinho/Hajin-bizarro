aea7ae5a4c090b5a6f11cebdb3b799ae657f81c5
Pulin <113272580+PulinPulinho@users.noreply.github.com>
2024-02-20 21:50:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const sim = document.querySelector('#sim');
 let sim_size = 10;
 
 nao.onclick = () => {
-    sim.style.width = (sim_size+1) + 'vw';
+    sim_size ++;
+    sim.style.width = '${sim_size}vw';
     nao.style.position = 'absolute';
     nao.style.left = '1vw';
-    nao.style.top = (Math.floor(Math.random()*50) + 1) + 'vh';
-    nao.style.left = (Math.floor(Math.random()*50) + 1) + 'vw';
+    nao.style.top = (Math.floor(Math.random() * 50) + 1) + 'vh';
+    nao.style.left = (Math.floor(Math.random() * 50) + 1) + 'vw';
 }
-sim.onclick = () => {
-    window.location = "muitobem.html"
-}
+
