840bec54f7ad1881ff993b92233c311fcbcce589
Pulin <113272580+PulinPulinho@users.noreply.github.com>
2024-02-20 21:53:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let sim_size = 10;
 
 nao.onclick = () => {
     sim_size ++;
-    sim.style.width = '${sim_size}vw';
+    sim.style.width = sim_size + 'vw';
     nao.style.position = 'absolute';
     nao.style.left = '1vw';
     nao.style.top = (Math.floor(Math.random() * 50) + 1) + 'vh';
